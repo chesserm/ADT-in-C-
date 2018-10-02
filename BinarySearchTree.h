7ccd0ffb205ccca3e39cf6fd222f640cf8e2e63d
chesserm <chesserm@umich.edu>
2018-10-02 08:54:09 -0400

    Changed variable names in BST member functions from root to input to avoid confusion with the Node * root member variable

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -22,8 +22,10 @@ public:
 
 
 	// Copy Constructor (performs a deep copy)
-	BinarySearchTree(const BinarySearchTree & right) : root{nullptr}, size{0} 
+	BinarySearchTree(const BinarySearchTree<T> & right) : root{nullptr}, size{0} 
 	{
+		// Deleting all nodes in the tree that will become a copy (just in case)
+		deleteAllNodes(root);
 		// Copy all nodes from the original to the copy
 		copyAllNodes(right.root);
 
@@ -31,7 +33,7 @@ public:
 
 
 	// Overriding assignment operator
-	BinarySearchTree & operator=(const BinarySearchTree & original)
+	BinarySearchTree & operator=(const BinarySearchTree<T> & original)
 	{
 		// Delete all nodes in the current tree
 		deleteAllNodes(root);
@@ -40,6 +42,7 @@ public:
 		copyAllNodes(original.root);
 
 	} //operator=()
+
 
 	// Returns true if the BST is empty
 	bool isEmpty ()
@@ -55,7 +58,7 @@ public:
 
 	// Returns the size of the BST 
 	// NOTE: If root is a nullptr, returns 0
-	size_t treeSize(Node * root)
+	size_t treeSize()
 	{
 		return size;
 	} //treeSize()
@@ -63,69 +66,69 @@ public:
 
 	// Returns the largest value in the BST by reference
 	// NOTE: This is just the value of the node furthest to the right in the BST
-	T & maxValue(Node * root)
+	T & maxValue(Node * input)
 	{
 		// Edge case (empty BST)
-		if (!root)
+		if (!input)
 		{
 			return 0;
 		} //if
 
 		// Base case
-		if (!root->right)
+		if (!input->right)
 		{
-			return root->datum;
+			return input->datum;
 		} //if base case
 
 		// Recursive Call
-		return maxValue(root->right);
+		return maxValue(input->right);
 
 	} //maxValue()
 
 
 	// Returns the smallest value in the BST by reference
 	// NOTE: This is just the value of the node furthest left in the BST
-	T & minValue(Node * root, T input)
+	T & minValue(Node * input, T input)
 	{
 		// Edge case (empty BST)
-		if (!root)
+		if (!input)
 		{
 			return 0;
 		} //if
 
 		// Base case
-		if (!root->left)
+		if (!input->left)
 		{
-			return root->datum;
+			return input->datum;
 		} //if base case
 
 
 		// Recursive Call
-		return minValue(root->left);
+		return minValue(input->left);
 
 	} //minValue()
 
 
 	// Inserts a node into the BST with the specified input value
-	void insertNode(Node * root, T input)
+	void insertNode(Node * nodeIn, T input)
 	{
 		// Base case
-		if (!root)
+		if (!nodeIn)
 		{
-			root = new Node;
-			root->datum = input;
+			nodeIn = new Node;
+			nodeIn->datum = input;
 
 		} //if
 	
 		// Insert to the left if the value is less than the current node
-		if (input < root->datum)
+		if (input < nodeIn->datum)
 		{
-			insertNode(root->left, input);
+			insertNode(nodeIn->left, input);
 		} //if
 		// Otherwise insert to the right
-		else if (input > root->datum)
+		else if (input > nodeIn->datum)
 		{
-			insertNode(root->right, input);
+			insertNode(nodeIn->right, input);
 		} //else if
 
 		// Increment size
@@ -134,26 +137,26 @@ public:
 
 
 	// Helper function for copy constructor and assignment operator overload
-	void copyAllNodes(Node * root)
+	void copyAllNodes(Node * input)
 	{
 		// Edge Case where root is nullptr
-		if (!root)
+		if (!input)
 		{
 			return;
 		} //if
 
 		// Traverse left and add each node
-		if (root->left)
+		if (input->left)
 		{
-			insertNode(root->datum);
-			copyAllNodes(root->left);
+			insertNode(input->datum);
+			copyAllNodes(input->left);
 		} //if
 
 		// Traverse right and add each node
-		if (root->right)
+		if (input->right)
 		{
-			copyAllNodes(root->right);
-			insertNode(root->datum);
+			copyAllNodes(input->right);
+			insertNode(input->datum);
 		}
 
 	} //copyAllNodes()
@@ -161,37 +164,37 @@ public:
 	// Returns the sum of the tree
 	// NOTE: This function is only really useful if the data type 
 	//       stored in each node is numerical.
-	T treeSum(Node * root)
+	T treeSum(Node * input)
 	{
 		// Edge case
-		if (!root)
+		if (!input)
 		{
 			return 0;
 		} //if
 
 		//Base case (found a leaf node)
-		if (!root->right && !root->left)
+		if (!input->right && !input->left)
 		{
-			return root->datum;
+			return input->datum;
 		} //if
 
 		// Recursive call to return the sum of the left and 
 		// right branches of the root node
-		return treeSum(root->left) + treeSum(root->right);
+		return treeSum(input->left) + treeSum(input->right);
 	} //treeSum()
 
 
 	// Returns the max value of the left side of the BST by reference
-	T & maxOfLeft(Node * root)
+	T & maxOfLeft(Node * input)
 	{
 		// Edge Case
-		if (!root)
+		if (!input)
 		{
 			return 0;
 		} //if
 
 		// Edge Case where only node in BST is the root
-		if (!root->left)
+		if (!input->left)
 		{
 			return 0;
 		} //if
@@ -200,22 +203,22 @@ public:
 		// we are finding the max of the subtree where the
 		// the first left node from the root is the root of the
 		// subtree.
-		return maxValue(root->left);
+		return maxValue(input->left);
 	} //maxOfLeft()
 
 
 	// Returns the min value of the right side of the BST by reference
-	T & minOfRight(Node * root)
+	T & minOfRight(Node * input)
 	{
 		// Edge Case
-		if (!root)
+		if (!input)
 		{
 			return 0;
 		} //if
 
 
 		// Edge case where only one node is in the BST
-		if (!root->right)
+		if (!input->right)
 		{
 			return 0;
 		} //if
@@ -224,7 +227,7 @@ public:
 		// we are finding the min of the subtree where the
 		// the first right node from the root is the root of the
 		// subtree.
-		return minValue(root->right);
+		return minValue(input->right);
 
 	} //minOfRight()
 
@@ -232,47 +235,47 @@ public:
 	// Prints the BST in in-order traversal
 	// Note: If this function is confusing, see the file
 	// 		 BST Recursion Explained.txt
-	void printInOrder(Node * root)
+	void printInOrder(Node * input)
 	{
 		// Base Case
-		if (!root)
+		if (!input)
 		{
 			return;
 		} //if
 
 		// Recurse to the left children
-		printInOrder(root->left);
+		printInOrder(input->left);
 
 		// Print values
-		cout << root->datum << " ";
+		cout << input->datum << " ";
 
 		// Recurse to right children
-		printInOrder(root->right);
+		printInOrder(input->right);
 
 	} //printBST()
 
 
 	// Prints the BST in pre-order traversal
-	void printPreOrder(Node * root)
+	void printPreOrder(Node * input)
 	{
 		// Base case
-		if (!root)
+		if (!input)
 		{
 			return;
 		} //if
 
 		// Traversing to the left of the subtree
-		if (root->left)
+		if (input->left)
 		{
-			std::cout << root->datum << " "; 
-			printPreOrder(root->left);
+			std::cout << input->datum << " "; 
+			printPreOrder(input->left);
 		} //if
 
 		// Traversing to the right of the subtree
-		if (root->right)
+		if (input->right)
 		{
-			std::cout << root->datum << " ";
-			printInOrder(root->right);
+			std::cout << input->datum << " ";
+			printInOrder(input->right);
 		} //if
 
 		// Flush stream
@@ -282,20 +285,20 @@ public:
 
 
 	// Helper function to delete the BST
-	void deleteAllNodes(Node * root)
+	void deleteAllNodes(Node * input)
 	{
 		// Base case
-		if (!root)
+		if (!input)
 		{
 			return;
 		} //if
 		
-		deleteAllNodes(root->left);
-		deleteAllNodes(root->right);
+		deleteAllNodes(input->left);
+		deleteAllNodes(input->right);
 		
-		root->left = nullptr;
-		root->right = nullptr;
-		delete root;
+		input->left = nullptr;
+		input->right = nullptr;
+		delete input;
 
 	} //deleteNode()
 
